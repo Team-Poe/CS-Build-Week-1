22ac8b4c9d6d5335aaf5cef686cb0bbd9de9f7c3
umbur <you@example.com>
2020-01-06 14:40:02 -0700

    Creates server using nodejs with some required dependencies

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+// sanity check
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Quizzer API is running" });
+});
+
+module.exports = server;
